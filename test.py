62eb47e53641f07573986635d5f406944f86d1fd
Phoenix <55042406+ThePhoenix78@users.noreply.github.com>
2022-09-22 22:34:52 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -21,23 +21,23 @@ test = []
 testy = []
 
 entre = []
-sort = []
+sortie = []
 
 for im in os.listdir(parapluie_path):
     entre.append(flatten(f"{parapluie_path}/{im}", x))
-    sort.append([1, 0, 0, 0])
+    sortie.append([1, 0, 0, 0])
 
 for im in os.listdir(ballon_path):
     entre.append(flatten(f"{ballon_path}/{im}", x))
-    sort.append([0, 1, 0, 0])
+    sortie.append([0, 1, 0, 0])
 
 for im in os.listdir(cacahuete_path):
     entre.append(flatten(f"{cacahuete_path}/{im}", x))
-    sort.append([0, 0, 1, 0])
+    sortie.append([0, 0, 1, 0])
 
 for im in os.listdir(bp_path):
     entre.append(flatten(f"{bp_path}/{im}", x))
-    sort.append([0, 0, 0, 1])
+    sortie.append([0, 0, 0, 1])
 
 
 training_set = []
@@ -67,28 +67,31 @@ for im in os.listdir(test_path):
     else:
         training_set.append([flatten(f"{test_path}/{im}", x), [0, 0, 0, 0]])
 
+
 # 2 hiddens layers (25 and 10 length)
 hidden_layers = [
      [25, "sigmoid"],
      [10, "sigmoid"]
 ]
 
-NN = NeuralNetwork(inputs=entre, results=sort, training_set=training_set, hidden_layers=hidden_layers, activation_function="sigmoid")
+NN = NeuralNetwork(inputs=entre, results=sortie, training_set=training_set, hidden_layers=hidden_layers, activation_function="sigmoid")
+
+NN.show_result()
 
 print("Starting training...")
 
-method = "genetic"
+method = "deep"
 epoch = 50
 if method == "deep":
     epoch = 500
 
-# a = NN.special_train(learning_method=method, population_size=25, learning_rate=1, cool=10, reset=True, max_retry=10, epoch=epoch, mutation=40, error=10, absolute_end=5)
+a = NN.special_train(learning_method=method, population_size=25, learning_rate=1, cool=10, reset=True, max_retry=10, epoch=epoch, mutation=50, error=10, absolute_end=5)
 # a = NN.smart_train(learning_method=method, population_size=25, learning_rate=1, cool=10, reset=True, max_retry=10, epoch=epoch, mutation=40, error=10)
-NN.genetic_train(population_size=50, epoch=150, error=10, threshold_error=0, mutation=40, method=2)
+# NN.genetic_train(population_size=50, epoch=150, error=10, threshold_error=0, mutation=40, method=2)
 # NN.deep_train(epoch=1500, learning_rate=1, error=10)
 
 
-NN.show_result(True)
+NN.show_result()
 
 
 if input("save? (y/n) : ") == "y":
